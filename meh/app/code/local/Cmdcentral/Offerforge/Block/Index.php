8f78486508713ce7dffe8a5a3a79223aa8777498
Calvin Muller w3 <calvin@istreet.co.za>
2012-10-03 12:44:32 +0200

    haha

---
+++ meh/app/code/local/Cmdcentral/Offerforge/Block/Index.php
@@ -0,0 +1,8 @@
+<?php   
+class Cmdcentral_Offerforge_Block_Index extends Mage_Core_Block_Template{   
+
+
+
+
+
+}
---
+++ meh/app/code/local/Cmdcentral/Offerforge/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Cmdcentral_Offerforge_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
