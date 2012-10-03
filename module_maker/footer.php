b90abd8656e83bdb34fcb5c17d325581faf57193
Calvin Muller w3 <calvin@istreet.co.za>
2012-10-03 12:33:56 +0200

    haha

--- module_maker/footer.php
+++
@@ -1,4 +0,0 @@
-		<span id="version">v.0.0.9.2 | <a href="http://www.magentocommerce.com/wiki/custom_module_with_custom_database_table" target="_blank">check for updates &amp; help</a></span>		
-	</div>
-</body>
-</html>
--- ok/app/code/local/Cmdcentral/PayD/Helper/Data.php
+++
@@ -1,4 +0,0 @@
-<?php
-/* ----- Copyright (c) CMDcentral 2012 ----- */
-
-class Cmdcentral_PayD_Helper_Data extends Mage_Payment_Helper_Data {}
